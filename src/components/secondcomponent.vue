bca63356af2fcc710e4df49567357fb8e8344c5c
gaokaomim <gaokaomim@163.com>
2017-07-17 15:46:32 +0800

    提交文件

---
+++ src/components/secondcomponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="secondcomponent">
+    <h1>I am another page</h1>
+    <a> written by {{ author }} </a>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      author: "xxxx",
+      articles: [],
+    }
+  }
+  
+}
+</script>
+
+<style>
+
+</style>
